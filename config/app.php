20c20f42046d41ecaa2e9f085ec01903eadc69f2
najims2016 <naji58809@gmail.com>
2025-08-17 18:32:05 +0300

    filament

--- config/app.php
+++ config/app.php
@@ -78,12 +78,12 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'ar'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
-
+    'faker_locale' => env('APP_FAKER_LOCALE', 'ar'),
+   // 'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
