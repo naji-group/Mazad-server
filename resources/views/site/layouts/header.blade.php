b0078002833d60c05ea5dd3526ddea405b381cb0
najims2016 <naji58809@gmail.com>
2025-10-13 23:58:09 +0300

    home

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -1,13 +1,55 @@
 <body>
-    <!-- HEADER -->
-    <header>
-    
+    	<!-- HEADER -->
+		<header>
+			<!-- TOP HEADER -->
+ 
+			<!-- /TOP HEADER -->
 
-     
-    </header>
-    <!-- /HEADER -->
+			<!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+					 
+						<!-- /LOGO -->
 
-    <!-- NAVIGATION -->
+						<!-- SEARCH BAR -->
+					 
+						<!-- /SEARCH BAR -->
+
+						<!-- ACCOUNT -->
+						<div class="col-md-3 clearfix">
+							<div class="header-ctn">
+								<!-- Wishlist -->
+								 
+								<!-- /Wishlist -->
+
+								<!-- Cart -->
+								 
+								<!-- /Cart -->
+
+								<!-- Menu Toogle -->
+								<div class="menu-toggle">
+									<a href="#">
+										<i class="fa fa-bars"></i>
+										<span>Menu</span>
+									</a>
+								</div>
+								<!-- /Menu Toogle -->
+							</div>
+						</div>
+						<!-- /ACCOUNT -->
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
+	<!-- NAVIGATION -->
     <nav id="navigation">
         <!-- container -->
         <div class="container">
@@ -15,17 +57,19 @@
             <div id="responsive-nav">
                 <!-- NAV -->
                 <ul class="main-nav nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li class="active"><a href="{{ url('/') }}">Home</a></li>
                     @foreach ($pages as $page )
                     <li  ><a href="{{ route('site.pages',$page->code) }}">{{ $page->name }}</a></li>
                     @endforeach
-                    
                 </ul>
                 <!-- /NAV -->
             </div>
-
             <!-- /responsive-nav -->
         </div>
         <!-- /container -->
     </nav>
+    <!-- /NAVIGATION -->
+
+    <!-- NAVIGATION -->
+
     <!-- /NAVIGATION -->
