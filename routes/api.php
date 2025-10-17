b5df37a9c202e9ab00600aeb58b2e2706d7dc2de
najims2016 <naji58809@gmail.com>
2025-10-17 19:41:21 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -34,13 +34,11 @@ Route::middleware('auth_marketer:api_marketers')->group(function () {
         Route::post('sendnotify', [MarketerController::class, 'sendnotify']); 
 
     });
-    Route::prefix('live')->group(function () {
- 
+    Route::prefix('live')->group(function () { 
         Route::post('savefaceaccesstoken', [LiveController::class, 'savefaceaccesstoken']); 
         Route::post('facebook/create', [LiveController::class, 'create_facebook_live']);
         Route::post('youtube/start-push', [LiveController::class, 'youtube_push']);
-        Route::post('youtube/stop-push', [LiveController::class, 'youtube_stop_push']);
-        
+        Route::post('youtube/stop-push', [LiveController::class, 'youtube_stop_push']);        
     });
 
 }); 
