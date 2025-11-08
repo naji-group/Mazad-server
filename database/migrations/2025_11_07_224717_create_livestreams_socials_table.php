50893f4f70963b7df2dee5fa89bcebfcb37a1de5
najims2016 <naji58809@gmail.com>
2025-11-08 21:34:41 +0300

    migrate

---
+++ database/migrations/2025_11_07_224717_create_livestreams_socials_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livestreams_socials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('live_stream_id')->nullable();
+$table->foreignId('social_id')->nullable();
+$table->integer('real_comments_count')->nullable();
+$table->integer('views_count')->nullable();
+$table->integer('likes_count')->nullable();
+$table->text('notes')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livestreams_socials');
+    }
+};
