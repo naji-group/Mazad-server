163a8369c01a5c101ae0744fdaec379e21061de7
najims2016 <naji58809@gmail.com>
2025-12-09 02:13:16 +0300

    Create logger.js

---
+++ public/tiktok/logger.js
@@ -0,0 +1,31 @@
+import winston from "winston";
+import path from "path";
+import fs from "fs";
+ 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const logDir = path.join(__dirname, "logs");
+//const logDir = "logs";
+
+// إنشاء المجلد إذا لم يكن موجودًا
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+}
+
+export function createLogger(livestreamId = "default") {
+    const logFile = path.join(logDir, `listener_${livestreamId}.log`);
+
+    return winston.createLogger({
+        level: "info",
+        format: winston.format.combine(
+            winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+            winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+        ),
+        transports: [
+            new winston.transports.File({ filename: logFile }),
+            new winston.transports.Console()
+        ]
+    });
+}
