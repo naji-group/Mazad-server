b2574fa4e0afab3f0efa7d9bae3675b6bef7a7f8
najims2016 <naji58809@gmail.com>
2026-01-05 17:04:38 +0300

    db

--- database/migrations/2026_01_05_131705_change_comments_count_in_live_streams_table.php
+++ database/migrations/2026_01_05_131705_change_comments_count_in_live_streams_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('live_streams', function (Blueprint $table) {
-             $table->integer('comments_count')->nullable();
+            $table->integer('comments_count')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('live_streams', function (Blueprint $table) {
-    
+            //
         });
     }
 };
